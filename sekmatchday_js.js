1b4446f6eca5e25f64cd6daa77fec8a51f548dd7
cfsek <joshua.mccloud@southeastkansas.org>
2024-10-03 11:55:53 -0500

    Add files via upload

---
+++ sekmatchday_js.js
@@ -0,0 +1,34 @@
+const header = document.querySelector('header');
+
+const secondaryColors = {
+    'cfsek' : '#74c69c',
+    'cacf' : '#cc2027',
+    'fsacf' : '#019f16',
+    'gacf' : '#8daa31'
+}
+
+const matchDayMinilogos = {
+    'cfsek' : document.getElementById('logo-cfsek'),
+    'cacf' : document.getElementById('logo-cacf'),
+    'fsacf' : document.getElementById('logo-fsacf'),
+    'gacf' : document.getElementById('logo-gacf')
+}
+
+const matchDayLinks = {
+    'cfsek' : document.getElementById('cfsek-link'),
+    'cacf' : document.getElementById('cacf-link'),
+    'fsacf' : document.getElementById('fsacf-link'),
+    'gacf' : document.getElementById('gacf-link')
+}
+
+for (const mdLink in matchDayLinks) {
+    matchDayLinks[mdLink].addEventListener('mouseenter', e => {
+        for (const minilogo in matchDayMinilogos) {
+            matchDayMinilogos[minilogo].classList.add('hidden');
+        }
+
+        matchDayMinilogos[mdLink].classList.remove('hidden');
+
+        header.style.backgroundColor = secondaryColors[mdLink];
+    });
+};
